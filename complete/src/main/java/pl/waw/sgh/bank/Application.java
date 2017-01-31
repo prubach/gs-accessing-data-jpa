8259482f87c847d1851bf6a2504b3a3f2678e3e6
Paweł Rubach <pawel.rubach@sorcersoft.com>
2017-01-31 23:46:11 +0100

    Updated to Spring Boot 1.5.1.RELEASE, simplified configuration, fixed bug with Vaadin Boot

--- complete/src/main/java/pl/waw/sgh/bank/Application.java
+++ complete/src/main/java/pl/waw/sgh/bank/Application.java
@@ -6,8 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
--- initial/src/main/java/pl/waw/sgh/bank/Application.java
+++ initial/src/main/java/pl/waw/sgh/bank/Application.java
@@ -6,8 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -16,8 +14,7 @@ public class Application {
 	private static final Logger log = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
-		ApplicationContext context = new AnnotationConfigApplicationContext(ApplicationConfig.class);
-		SpringApplication.run(Application.class);
+		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
