dfa24a6c929644c0f6310211aa9250efdeb120fb
Paweł Rubach <pawel.rubach@sorcersoft.com>
2020-04-02 12:41:59 +0200

    Added a REST exposed search method for account

--- complete/src/main/java/pl/waw/sgh/bank/AccountRepository.java
+++ complete/src/main/java/pl/waw/sgh/bank/AccountRepository.java
@@ -4,12 +4,20 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "account", path = "account")
 public interface AccountRepository extends PagingAndSortingRepository<Account, Long> {
 
+    /*
+        Example call:
+
+        http://localhost:8080/account/search/findByCustomer?customer=http://localhost:8080/customer/1
+     */
+
+    @RestResource(path = "findByCustomer", rel = "findByCustomer")
     List<Account> findByCustomer(@Param("customer") Customer customer);
 
     Account findByAccountID(@Param("id") Long accountId);
